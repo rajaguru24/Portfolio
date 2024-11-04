a35342e3334fa2140584c9c6ae21dbfd79a96cfa
rajaguru24 <rajaguru246@gmail.com>
2024-11-04 18:14:08 +0530

    updated

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><div className='content_container'>
+    <h1 className='w-full text-green-400 text-4xl leading-relaxed'>Welcome,<br/>I'm Rajaguru
+    <p> I am a Full Stack Developer</p>
+
+    </h1>
+    <p className="text-yellow-300 text-2xl">I am proficient in React JS, Node JS and MongoDB, using these as Frontend, Backend and Database, I will create websites in my unique style... </p>        
+</div>
+
+    </div>
+  )
+}
+
+export default About
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,14 +14,8 @@ const Header=()=>{
         <Link to="projects"><h4>Projects</h4></Link>
         <Link to ='resume'><h4>Resume</h4></Link>
         <Link to ="contact"><h4>Contact</h4></Link>
-        </div><br/><br/>
-        <div className='content_container'>
-        <h1 className='w-full text-green-400 text-4xl leading-relaxed'>Welcome,<br/>I'm Rajaguru
-        <p> I am a Full Stack Developer</p>
-
-        </h1>
-        <p className="text-yellow-300 text-2xl">I am proficient in React JS, Node JS and MongoDB, using these as Frontend, Backend and Database, I will create websites in my unique style... </p>        
-    </div>
+        </div>
+        
       </div>
     </div>
 
