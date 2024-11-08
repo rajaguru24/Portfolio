4c7f2db5dc210ab34aa4b9d8ce6cea23b0243c3a
rajaguru24 <rajaguru246@gmail.com>
2024-11-08 11:35:29 +0530

    updated

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -10,7 +10,7 @@ export default function Resume(){
 <div className='flex flex-col'>
   <h1 className=' text-3xl border-b-4 border-purple-400'>Resume</h1>
 <div className='pb-5'>
-You can view my resume  <a className ='btn 'href='https://drive.google.com/file/d/1GWTF8X7J6yjnF78L9Pchyk7b1V-RILi4/view?usp=sharing'>Download</a>
+You can view my resume  <a className ='btn 'href='https://drive.google.com/file/d/1kWjdta1-z3CzHGC-SQu3RW9JVHYmue2u/view?usp=sharing'>Download</a>
 </div>
 </div>
 </div>
